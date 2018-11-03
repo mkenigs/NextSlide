6a1c4d0084f1c8f5083356b413f54b543367fe87
William Wu <admin@William-Ws-MacBook-Pro.local>
2018-11-03 15:47:19 -0500

    GUI commit

--- NextSlide/mainwindow.py
+++ NextSlide/mainwindow.py
@@ -1,18 +1,29 @@
-import sys
-import transcription
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Window(QtWidgets.QMainWindow):
-
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("NextSlide")
-        self.show()
+import tkinter as tk
 
 
-if __name__ == '__main__':
-   transcription.main()
-	# app = QtWidgets.QApplication(sys.argv)
-	# GUI = Window()
-	# sys.exit(app.exec_())
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack(padx=135, pady=115)
+        self.photo = tk.PhotoImage(file="play_black.gif")
+        self.play = tk.Button(self)
+        self.play["text"] = "Start Listening"
+        #self.play["image"] = self.photo
+        self.play["fg"] = "red"
+        self.play["height"] = 5
+        self.play["width"] = 20
+        self.play["command"] = self.run
+        self.play.bind("<Enter>", self.color)
+        self.play.pack(side='top')
+
+    def run(self):
+        print("hi there, everyone!")
+
+
+    def color(self, event):
+        event.widget["activeforeground"] = "blue"
+
+
+root = App()
+root.master.title("NextSlide")
+root.mainloop()
