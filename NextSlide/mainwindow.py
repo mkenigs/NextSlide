c1fa3392af26897de5e232aa3d4e7866cc475668
matthew <matthewkenigsberg@gmail.com>
2018-11-03 03:38:04 -0500

    created files

---
+++ NextSlide/mainwindow.py
@@ -0,0 +1,18 @@
+import sys
+import processing
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("NextSlide")
+        self.show()
+
+
+if __name__ == '__main__':
+   processing.main()
+	# app = QtWidgets.QApplication(sys.argv)
+	# GUI = Window()
+	# sys.exit(app.exec_())
