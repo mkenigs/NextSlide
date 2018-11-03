c6395b6db60cf7e691e49262e5b0a6f9080ff9bb
matthew <matthewkenigsberg@gmail.com>
2018-11-03 15:14:02 -0500

    removed if name == main

--- NextSlide/Powerpoint2Text.py
+++ NextSlide/Powerpoint2Text.py
@@ -48,8 +48,3 @@ def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
     return sorted(l, key = alphanum_key)
-
-
-
-if __name__ == "__main__":
-    arr = parsePPTX("test.pptx")
