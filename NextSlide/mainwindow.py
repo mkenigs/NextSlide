108104cab6962c3774e85aebae65ecb9076ac5ae
matthew <matthewkenigsberg@gmail.com>
2018-11-03 21:29:28 -0500

    oop mainwindow

--- NextSlide/mainwindow.py
+++ NextSlide/mainwindow.py
@@ -1,66 +1,62 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
-from transcription import Transcriber
+import transcription
 
 def read(input):
     text = input
 
+class MainWindow():
+    def __init__(self, master):
+        self.master = master
+        self.master.title('NextSlide')
+        self.master.geometry('200x160')
+        self.master.resizable(0, 0)
 
-def startListening():
-    if not play['text']:
-        text1['text'] = 'Please select a file first'
-    else:
-        text1['text'] = 'Listening'
-        play['image'] = pause
-        myTranscriber.start()
-        if myTranscriber:
-            print(1)
+        self.text1 = Label(text='Press play to start')
+        self.text1.pack(padx=10, pady=10)
+
+        self.start = PhotoImage(file='Play.png')
+        self.pause = PhotoImage(file='Pause.png')
+        self.play = Button(image=self.start, height=50, width=50, command=self.run)
+        self.play.pack(padx=10, pady=10)
+
+        self.pickFile = Button(text='Please select a file', height=50, width=50, command=self.getFile)
+        self.pickFile.pack(padx=10, pady=10)
+
+        self.myTranscriber = None
+
+
+    def startListening(self):
+        if not self.play['text']:
+            self.text1['text'] = 'Please select a file first'
         else:
-            print(0)
+            self.text1['text'] = 'Listening'
+            self.play['image'] = self.pause
+            self.myTranscriber.start()
 
 
-def stopListening():
-    play['image'] = start
-    text1['text'] = 'Press play to start'
-    myTranscriber.stop()
-    if myTranscriber:
-        print(1)
-    else:
-        print(0)
+    def stopListening(self):
+        self.play['image'] = self.start
+        self.text1['text'] = 'Press play to start'
+        if self.myTranscriber: self.myTranscriber.stop()
 
 
-def run():
-    if play['image'] == start.name:
-        startListening()
-    else:
-        stopListening()
+    def run(self):
+        if self.play['image'] == self.start.name:
+            self.startListening()
+        else:
+            self.stopListening()
 
 
-def getFile():
-    play['text'] = askopenfilename()
-    global myTranscriber
-    if play['text']:
-        pickFile['text'] = play['text'][play['text'].rfind('/') + 1:]
-        stopListening()
-        myTranscriber = Transcriber(play['text'])
-    else:
-        myTranscriber = None
+    def getFile(self):
+        self.play['text'] = askopenfilename()
+        if self.play['text']:
+            self.pickFile['text'] = self.play['text'][self.play['text'].rfind('/') + 1:]
+            self.stopListening()
+            self.myTranscriber = transcription.Transcriber(self.play['text'])
 
 
-master = Tk()
-master.title('NextSlide')
-master.geometry('200x160')
-master.resizable(0, 0)
-
-text1 = Label(text='Press play to start')
-text1.pack(padx=10, pady=10)
-
-start = PhotoImage(file='Play.png')
-pause = PhotoImage(file='Pause.png')
-play = Button(image=start, height=50, width=50, command=run)
-play.pack(padx=10, pady=10)
-
-pickFile = Button(text='Please select a file', height=50, width=50, command=getFile)
-pickFile.pack(padx=10, pady=10)
-
-mainloop()
+if __name__ == '__main__':
+    master = Tk()
+    myGui = MainWindow(master)
+    master.mainloop()
